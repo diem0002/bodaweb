07c344f33e0b0bfde5bb75f04ea89f6a08b3e924
diem0002 <davidmover0002@gmail.com>
2025-11-28 10:09:06 -0300

    actualizacion de datos calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,10 +1,10 @@
 function agendarEnCalendario() {
-    const fechaBoda = '20251126T230000';
-    const fechaFin = '20251126T230000';
+    const fechaBoda = '20261121T230000';
+    const fechaFin = '20261122T230000';
     
     const detalles = {
         title: 'Boda de Florencia & Joaquin',
-        description: '¡No te pierdas nuestro día especial! Ceremonia a las 16:00 y recepción a las 19:00.',
+        description: '¡No te pierdas nuestro día especial! Ceremonia a las 18:00 y recepción a las 19:00.',
         location: 'Iglesia San José, Av. Siempre Viva 123, Ciudad',
         start: fechaBoda,
         end: fechaFin
